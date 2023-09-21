6d4b145a6819ea544f0dc9a21d19547c81e30622
J. R. Equipamentos <lunadream354@gmail.com>
2023-09-21 02:57:11 -0300

    first commit

---
+++ LinkedList.h
@@ -0,0 +1,230 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include <stdbool.h>
+#include <stdlib.h>
+typedef struct LinkedNode {
+    void *data;
+    bool isUsed;
+    struct LinkedNode *next;
+} LinkedNode;
+
+LinkedNode *listCreate(void *data, bool initialized);
+LinkedNode *listPush(LinkedNode *list, void *data);
+LinkedNode *listAddAt(LinkedNode *list, void *data, size_t position);
+void *listPop(LinkedNode *list);
+void *listRemoveAt(LinkedNode *list, size_t position);
+// void** listRemoveRange(LinkedNode *list, size_t start,size_t end);
+void *listGetAt(LinkedNode *list, size_t position);
+LinkedNode *listGetNodeAt(LinkedNode *list, size_t position);
+size_t listSize(LinkedNode *list, bool *isCIrcular);
+void listDelete(LinkedNode **list);
+void listSort(LinkedNode *list, bool (*func)(void *, void *));
+bool listSearch(LinkedNode *list, size_t *result, void *search, bool (*searchFunc)(void *, void *));
+bool listIsNodeValid(LinkedNode *node);
+
+LinkedNode *listCreate(void *data, bool initialized) {
+    LinkedNode *head = (LinkedNode *)malloc(sizeof(LinkedNode));
+    head->data = data;
+    head->isUsed = initialized;
+    head->next = NULL;
+    return head;
+}
+LinkedNode *listPush(LinkedNode *list, void *data) {
+    if (list->isUsed) {
+        LinkedNode *node = listCreate(list->data, true);
+        list->data = data;
+        list->next = node;
+    } else {
+        list->isUsed = true;
+        list->data = data;
+        list->next = NULL;
+    }
+    return list;
+}
+
+LinkedNode *listAddAt(LinkedNode *list, void *data, size_t position) {
+    if (!position)
+        return listPush(list, data);
+
+    LinkedNode *nodeCurrent = list;
+    LinkedNode *nodeNext = list;
+    for (size_t i = 0; i < position; i++) {
+        nodeCurrent = nodeNext;
+        nodeNext = nodeNext->next;
+    }
+    LinkedNode *node = listCreate(data, true);
+    nodeCurrent->next = node;
+    node->next = nodeNext;
+    return list;
+}
+
+void *listPop(LinkedNode *list) {
+    if (list->isUsed == false)
+        return NULL;
+    LinkedNode *nodeLast = list;
+    LinkedNode *nodePenult = list;
+    void *data = NULL;
+    while (nodeLast->next != NULL) {
+        nodePenult = nodeLast;
+        nodeLast = nodeLast->next;
+    }
+    data = nodeLast->data;
+    nodePenult->next = NULL;
+    if (nodeLast == nodePenult)
+        nodeLast->isUsed = false;
+    else
+        free(nodeLast);
+    return data;
+}
+void *listRemoveAt(LinkedNode *list, size_t position) {
+    if (!list->isUsed)
+        return NULL;
+    LinkedNode *nodeBefore = NULL;
+    LinkedNode *nodeCurrent = NULL;
+    LinkedNode *nodeAfter = list;
+    for (size_t i = 0; i < position + 1; i++) {
+        nodeBefore = nodeCurrent;
+        nodeCurrent = nodeAfter;
+        nodeAfter = nodeAfter->next;
+    }
+    if (nodeBefore != NULL)
+        nodeBefore->next = nodeAfter;
+    void *data = nodeCurrent->data;
+
+    if (nodeAfter == NULL) {      // ultimo elemento
+        if (nodeBefore == NULL) { // unico elemento
+            nodeCurrent->data = NULL;
+            nodeCurrent->isUsed = false;
+        } else { // nao e o unico elemento
+            nodeBefore->next = NULL;
+            free(nodeCurrent);
+        }
+    } else {                      // nao e o ultimo elemento
+        if (nodeBefore == NULL) { // primeiro elemento
+            size_t size = listSize(list, NULL);
+            LinkedNode *crawlerAfter = list->next; 
+            LinkedNode *crawler = list;
+            for (size_t i = 0; i < size; i++) {
+                crawler->data = crawlerAfter->data;
+                crawler->next = crawlerAfter->next;
+                crawler = crawlerAfter;
+                crawlerAfter = crawlerAfter->next;
+            }
+            crawler->next = NULL;
+            free(crawlerAfter);
+        } else {
+            nodeBefore->next = nodeAfter;
+            nodeCurrent->next = NULL;
+            nodeCurrent->data = NULL;
+            free(nodeCurrent);
+        }
+    }
+    return data;
+}
+/*
+void **listRemoveRange(LinkedNode *list, size_t start, size_t end) {
+    size_t size = end - start + 1;
+    void **listOut = (void **)malloc(sizeof(void *) * (size));
+    LinkedNode *nodeStart = list;
+    for (size_t i = 0; i < start - 1; i++)
+        nodeStart = nodeStart->next;
+    LinkedNode *nodeCurrent = nodeStart;
+    for (size_t i = 0; i <size;i++){
+        listOut[i] = nodeCurrent->data;
+        nodeCurrent = nodeCurrent->next;
+    }
+}
+*/
+void *listGetAt(LinkedNode *list, size_t position) {
+    LinkedNode *node = list;
+    for (size_t i = 0; i < position; i++) {
+        node = node->next;
+    }
+    return node->data;
+}
+LinkedNode *listGetNodeAt(LinkedNode *list, size_t position) {
+    for (size_t i = 0; i < position; i++) {
+        list = list->next;
+    }
+    return list;
+}
+
+size_t listSize(LinkedNode *list, bool *isCircular) {
+    bool isCircularTemp = false;
+    bool *isCirculatTempPtr = &isCircularTemp;
+    bool **isCircularPtr = &isCirculatTempPtr;
+    if (isCircular)
+        isCircularPtr = &isCircular;
+    if (!list->isUsed) {
+        (**isCircularPtr) = false;
+        return 0;
+    }
+
+    size_t size = 1;
+    LinkedNode *nodeStart = list;
+    LinkedNode *node = nodeStart;
+    while (node != NULL && node->next != NULL && node->next != nodeStart) {
+        size++;
+        node = node->next;
+    }
+    (**isCircularPtr) = node->next != NULL;
+    return size;
+}
+void listDelete(LinkedNode **list) {
+    if (!(*list)->next) {
+        free(list);
+        list = NULL;
+        return;
+    }
+    LinkedNode *node = *list;
+    LinkedNode *nodeNext = (*list)->next;
+    do {
+        free(node);
+        node = nodeNext->next;
+        nodeNext = node->next;
+    } while (node);
+    *list = NULL;
+}
+void listSort(LinkedNode *list, bool (*organizer)(void *, void *)) {
+    size_t size = listSize(list, NULL);
+    if (size <= 1)
+        return;
+
+    for (size_t j = 0; j < size - 1; j++) {
+        LinkedNode *search = listGetNodeAt(list, j); // ineficiente mas depois eu mudo
+        for (size_t i = j + 1; i < size; i++) {
+            LinkedNode *crawler = listGetNodeAt(list, i); // ineficiente mas depois eu mudo
+            if (!organizer(search->data, crawler->data))
+                continue;
+            LinkedNode *searchBefore = NULL;
+            LinkedNode *crawlerBefore = NULL;
+            if (j) {
+                searchBefore = listGetNodeAt(list, j - 1);
+                searchBefore->next = crawler;
+            }
+            if (j != i - 1) {
+                crawlerBefore = listGetNodeAt(list, i - 1);
+                crawlerBefore->next = search;
+            }
+            LinkedNode *tmp = crawler->next;
+            crawler->next = search->next;
+            search->next = tmp;
+        }
+    }
+}
+bool listSearch(LinkedNode *list, size_t *result, void *search, bool (*searchFunc)(void *, void *)) {
+    size_t size = listSize(list, NULL);
+    for (size_t i = 0; i < size; i++) {
+        if (searchFunc(search, list->data)) {
+            if (result)
+                *result = i;
+            return true;
+        }
+        list = list->next;
+    }
+    return false;
+}
+bool listIsNodeValid(LinkedNode *node) {
+    return node->isUsed;
+}
+#endif
